3353aed87ec7eae0356fd8cd4378c7a4c15dc559
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-10-01 06:10:36 -0300

    Initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StartButtonComponent } from './start-button/start-button.component';
+
+
+
+@NgModule({
+  declarations: [StartButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
