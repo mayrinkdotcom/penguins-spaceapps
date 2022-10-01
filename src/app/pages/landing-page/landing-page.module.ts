acee2276f3f4727160f2c6edb64d5beea716c51d
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2022-10-01 06:49:53 -0300

    feat: create basic structure for landing page

---
+++ src/app/pages/landing-page/landing-page.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    LandingPageComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+  ],
+  exports: [LandingPageComponent]
+})
+export class LandingPageModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartButtonComponent } from './start-button/start-button.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [StartButtonComponent],
+  declarations: [StartButtonComponent, HeaderComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    HeaderComponent,
+    StartButtonComponent,
+  ],
 })
 export class SharedModule { }
